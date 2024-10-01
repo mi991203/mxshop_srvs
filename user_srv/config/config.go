5928aa1f1f2308a6ed2cd8045ff9a50cc213e245
romanshao <romanshao@tencent.com>
2024-10-01 22:04:08 +0800

    将user-srv注册到consul，并提供健康检查的功能

--- user_srv/config/config.go
+++ user_srv/config/config.go
@@ -14,7 +14,7 @@ type ConsulConfig struct {
 }
 
 type ServerConfig struct {
-	//Name       string       `mapstructure:"name" json:"name"`
+	Name       string       `mapstructure:"name" json:"name"`
 	MysqlInfo  MysqlConfig  `mapstructure:"mysql" json:"mysql"`
 	ConsulInfo ConsulConfig `mapstructure:"consul" json:"consul"`
 }
--- user_srv/main.go
+++ user_srv/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/hashicorp/consul/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+	"mxshop_srvs/user_srv/global"
 	"mxshop_srvs/user_srv/handler"
 	"mxshop_srvs/user_srv/initialize"
 	"mxshop_srvs/user_srv/proto"
 	"net"
-
-	"google.golang.org/grpc"
 )
 
 func main() {
@@ -28,6 +31,39 @@ func main() {
 	if err != nil {
 		panic("fialed to listen:" + err.Error())
 	}
+
+	//注册服务健康检查
+	grpc_health_v1.RegisterHealthServer(server, health.NewServer())
+
+	// 服务注册
+	consulConfig := api.DefaultConfig()
+	consulConfig.Address = "127.0.0.1:8500"
+
+	client, err := api.NewClient(consulConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	// 添加健康检查
+	check := &api.AgentServiceCheck{
+		GRPC:                           "192.168.0.109:50051",
+		Timeout:                        "5s",
+		Interval:                       "5s",
+		DeregisterCriticalServiceAfter: "15s",
+	}
+
+	registration := new(api.AgentServiceRegistration)
+	registration.Address = "192.168.0.109"
+	registration.Port = *Port
+	registration.Name = global.ServerConfig.Name
+	registration.Tags = []string{"mxshop"}
+	registration.ID = global.ServerConfig.Name
+	registration.Check = check
+
+	if err = client.Agent().ServiceRegister(registration); err != nil {
+		panic(err)
+	}
+
 	if err := server.Serve(lis); err != nil {
 		panic("fail to start grpc:" + err.Error())
 	}
